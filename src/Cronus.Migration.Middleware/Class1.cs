84f5617eb4bb190879c8cd6b6d45a4b705544fca
SayliS <rssaylis@gmail.com>
2016-06-07 11:09:21 +0300

    Batman! (this commit has no parents)

---
+++ src/Cronus.Migration.Middleware/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cronus.Migration.Middleware
+{
+    public class Class1
+    {
+    }
+}
